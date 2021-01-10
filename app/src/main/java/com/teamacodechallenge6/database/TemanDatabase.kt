9d9dcddd4f668aa442cb1c8bacab31f96c7852f7
Agung Wicaksono <agung.awd@gmail.com>
2021-01-10 19:52:31 +0700

    tambah database

---
+++ app/src/main/java/com/teamacodechallenge6/database/TemanDatabase.kt
@@ -0,0 +1,28 @@
+package com.teamacodechallenge6.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Teman::class], version = 1)
+abstract class TemanDatabase :RoomDatabase() {
+    abstract fun temanDao() : TemanDao
+
+    companion object{
+        private var INSTANCE : TemanDatabase? =null
+
+        fun getInstance(context: Context): TemanDatabase? {
+            if (INSTANCE == null){
+                synchronized(TemanDatabase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    TemanDatabase::class.java, "Teman.db").build()
+                }
+            }
+            return INSTANCE
+        }
+        fun destroyInstance(){
+            INSTANCE = null
+        }
+    }
+}
