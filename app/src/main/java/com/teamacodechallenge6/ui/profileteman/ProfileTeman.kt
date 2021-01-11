2c7b83a1d764ef3e27e02a0d6b6608a98a4ef21f
Junjungan Siregar <67586070+boymilo@users.noreply.github.com>
2021-01-11 14:34:41 +0700

    V1.1 from lukas
    Memperbaikin manifest dan menambah beberapa res

---
+++ app/src/main/java/com/teamacodechallenge6/ui/profileteman/ProfileTeman.kt
@@ -0,0 +1,24 @@
+package com.teamacodechallenge6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.teamacodechallenge6.database.TemanDatabase
+
+class ProfileTeman : AppCompatActivity() {
+    private var mDB : TemanDatabase? = null
+    private var recyclerView: RecyclerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_teman)
+        mDB = TemanDatabase.getInstance(this)
+
+
+        recyclerView?.layoutManager = LinearLayoutManager(
+            this, LinearLayoutManager.VERTICAL, false)
+
+
+    }
+}
